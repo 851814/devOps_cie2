225aa157da854fd77089dfb7cc6182b72f4d8702
soumya-sunkad <soumyasunkad06@gmail.com>
2025-11-29 10:58:10 +0530

    save

--- file1.py
+++ file1.py
@@ -1,11 +1,10 @@
 import sys
-if(sys.argv)!=4:
+if(sys.argv)!=3:
    print("error:Python file1.py number,name,age & birth")
    sys.exit(1)
    num=int(sys.argv[1])
    name=int(sys.argv[2])
    age=int(sys.argv[3])
-   birth=int(sys.argv[4])
    print("Train Ticket Reservation:")
    print("Train Number:",num)
    print("Name of the passenger:",name)
